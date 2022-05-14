8fe7a859edec24af15f1e902155883f8456839c8
Fernando Dodino <fernando.dodino@gmail.com>
2022-05-14 10:33:34 -0300

    Adding profile

--- src/test/java/com/example/demo/PedidoTests.java
+++ src/test/java/com/example/demo/PedidoTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfile("test")
 class PedidoTests {
 
 	@Autowired
